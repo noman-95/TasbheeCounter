dc0108c136d34828ee3dba5fa891cd9be5d1acbe
noman-95 <algorithmtales69@gmail.com>
2026-09-18 18:21:28 +0400

    Update Kotlin plugin version and formatting

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,7 +4,9 @@ pluginManagement {
             val properties = java.util.Properties()
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            require(flutterSdkPath != null) {
+                "flutter.sdk not set in local.properties"
+            }
             flutterSdkPath
         }
 
@@ -20,7 +22,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
